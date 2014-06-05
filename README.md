69782818420135d0500a3dcd6b67dc9630e41dc3
Code Ass <aycabta@gmail.com>
2014-06-05 23:50:07 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Google Direct Linker
+====================
+
+Skip redirection URL on Google search results.
+
+[This Greasemonkey script on OpenUserJS.org](https://openuserjs.org/scripts/aycabta/httpaycabta.github.io/Google_Direct_Linker)
+
---
+++ google-direct-linker.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name       Google Direct Linker
+// @namespace  http://aycabta.github.io/
+// @version    0.1.0
+// @description  Skip redirection URL on Google search results.
+// @include    /https?:\/\/www.google\.[^\/]+\/search.+/
+// @copyright  2014+, Code Ass
+// ==/UserScript==
+
+(function() {
+    var i;
+    var links = document.getElementsByTagName("a");
+    for (i = 0; i < links.length; i++) {
+        var link = links[i];
+        if (link.hasAttribute("onmousedown")) {
+            link.removeAttribute("onmousedown");
+            if(link.removeEventListener){
+                link.removeEventListener("mousedown", link.onmousedown, false);
+            }else if(link.detachEvent){
+                link.detachEvent("onmousedown", link.onmousedown);
+            }
+        }
+    }
+})();
+
